fe1b7dc2d44a5e11996418b690cd6b36854e1fd3
Parker Mathis <parkerjmathis@gmail.com>
2024-03-21 19:19:11 -0700

    Add icons to subject list

--- src/components/SubjectList.tsx
+++ src/components/SubjectList.tsx
@@ -1,17 +1,71 @@
-const SubjectList = () => {
-  const subjects = [
-    { id: 1, name: "Fiction", search: "fiction" },
-    { id: 2, name: "Drama", search: "drama" },
-    { id: 3, name: "Fantasy", search: "fantasy" },
-    { id: 4, name: "Science Fiction", search: "science+fiction" },
-  ];
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 
+import fictionIconLight from "../assets/fictionIconLight.png";
+import fictionIconDark from "../assets/fictionIconDark.png";
+import dramaIconLight from "../assets/dramaIconLight.png";
+import dramaIconDark from "../assets/dramaIconDark.png";
+import fantasyIconLight from "../assets/fantasyIconLight.png";
+import fantasyIconDark from "../assets/fantasyIconDark.png";
+import scienceIconLight from "../assets/scienceIconLight.png";
+import scienceIconDark from "../assets/scienceIconDark.png";
+
+const SubjectList = () => {
+  const { colorMode } = useColorMode();
+
+  const subjects = [
+    {
+      id: 1,
+      name: "Fiction",
+      search: "fiction",
+      iconLight: fictionIconLight,
+      iconDark: fictionIconDark,
+    },
+    {
+      id: 2,
+      name: "Drama",
+      search: "drama",
+      iconLight: dramaIconLight,
+      iconDark: dramaIconDark,
+    },
+    {
+      id: 3,
+      name: "Fantasy",
+      search: "fantasy",
+      iconLight: fantasyIconLight,
+      iconDark: fantasyIconDark,
+    },
+    {
+      id: 4,
+      name: "Science Fiction",
+      search: "science+fiction",
+      iconLight: scienceIconLight,
+      iconDark: scienceIconDark,
+    },
+  ];
+  console.log(colorMode);
   return (
-    <ul>
+    <List>
       {subjects.map((subject) => (
-        <li key={subject.id}>{subject.name}</li>
+        <ListItem key={subject.id} paddingTop={2}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={0}
+              src={colorMode === "dark" ? subject.iconLight : subject.iconDark}
+              objectFit="contain"
+            />
+            <Text>{subject.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
